e12ad6d1757fdea61aa504d66d0cfa07ba3f4655
johnpham <pltnhan311@gmail.com>
2023-12-15 23:35:52 +0700

    updated code componentdidmount

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
+const lists = ['BWM', 'Audi', 'Mayback']
+
+const fetchApi = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(lists)
+    }, 1000)
+  })
+}
 export default class Clock extends React.Component {
   constructor(props) {
     super(props)
@@ -9,8 +18,18 @@ export default class Clock extends React.Component {
       },
       seconds: {
         created: new Date().getSeconds()
-      }
+      },
+      lists: []
     }
+  }
+
+  componentDidMount() {
+    const timeElement = document.getElementById('time')
+    console.log(timeElement);
+    fetchApi().then(res => this.setState(prevState => ({
+      ...prevState,
+      lists: res
+    })))
   }
 
   getTime = () => {
@@ -27,10 +46,11 @@ export default class Clock extends React.Component {
   }
  
   render() {
+    console.log(this.state);
     return (
       <div>
         <h1>Hello World</h1>
-        <h2>It is {this.state.time.created}</h2>
+        <h2 id='time'>It is {this.state.time.created}</h2>
         <h3>Seconds: {this.state.seconds.created}</h3>
         <button onClick={this.getTime}>Get Time</button>
       </div>
